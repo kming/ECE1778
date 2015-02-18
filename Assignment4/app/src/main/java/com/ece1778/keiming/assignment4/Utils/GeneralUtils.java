6ce17c717ba0dc9dac0966dfd0fc2ec39a85d142
kming <Keiming.kwong@gmail.com>
2015-02-18 12:17:14 -0500

    Adding Assignment 4 and Project Folder

---
+++ Assignment4/app/src/main/java/com/ece1778/keiming/assignment4/Utils/GeneralUtils.java
@@ -0,0 +1,24 @@
+package com.ece1778.keiming.assignment4.Utils;
+
+import java.util.Calendar;
+
+/**
+ * Created by Kei-Ming on 2015-02-10.
+ */
+public class GeneralUtils {
+    private GeneralUtils() {}
+
+    public static String getDateString () {
+        return  Integer.toString(Calendar.getInstance().get(Calendar.DAY_OF_MONTH)) + "/" +
+                Integer.toString(Calendar.getInstance().get(Calendar.MONTH)) + "/" +
+                Integer.toString(Calendar.getInstance().get(Calendar.YEAR));
+    }
+    public static String getDateTimeString () {
+        return  Integer.toString(Calendar.getInstance().get(Calendar.DAY_OF_MONTH)) + "/" +
+                Integer.toString(Calendar.getInstance().get(Calendar.MONTH)) + "/" +
+                Integer.toString(Calendar.getInstance().get(Calendar.YEAR)) + " " +
+                Integer.toString(Calendar.getInstance().get(Calendar.HOUR_OF_DAY)) + ":" +
+                Integer.toString(Calendar.getInstance().get(Calendar.MINUTE));
+
+    }
+}
