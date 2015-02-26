7ee3c8fe7c3396e4abdb7ea03d4870d7cd1b8b7e
kming <Keiming.kwong@gmail.com>
2015-02-26 00:10:36 -0500

    updated files

--- Footprints/app/src/main/java/com/ece1778/keiming/footprints/Models/BaseDatabase.java
+++ Footprints/app/src/main/java/com/ece1778/keiming/footprints/Models/BaseDatabase.java
@@ -3,5 +3,10 @@ package com.ece1778.keiming.footprints.Models;
 /**
  * Created by Kei-Ming on 2015-02-24.
  */
-public class BaseDatabase {
-}
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Kei-Ming on 2015-02-18.
+ */
+public class BaseDatabase
--- Footprints/app/src/main/java/com/ece1778/keiming/footprints/Utils/GeneralUtils.java
+++ Footprints/app/src/main/java/com/ece1778/keiming/footprints/Utils/GeneralUtils.java
@@ -1,7 +1,27 @@
 package com.ece1778.keiming.footprints.Utils;
 
+import java.util.Calendar;
+
 /**
  * Created by Kei-Ming on 2015-02-24.
  */
 public class GeneralUtils {
+
+    public static final String TAG = GeneralUtils.class.getName(); // Debug Tag
+
+    private GeneralUtils () { /* empty constructor */ } // Used as a package of utility functions
+
+    public static String getDateString () {
+        return  Integer.toString(Calendar.getInstance().get(Calendar.DAY_OF_MONTH)) + "/" +
+                Integer.toString(Calendar.getInstance().get(Calendar.MONTH)) + "/" +
+                Integer.toString(Calendar.getInstance().get(Calendar.YEAR));
+    }
+    public static String getDateTimeString () {
+        return  Integer.toString(Calendar.getInstance().get(Calendar.DAY_OF_MONTH)) + "/" +
+                Integer.toString(Calendar.getInstance().get(Calendar.MONTH)) + "/" +
+                Integer.toString(Calendar.getInstance().get(Calendar.YEAR)) + " " +
+                Integer.toString(Calendar.getInstance().get(Calendar.HOUR_OF_DAY)) + ":" +
+                Integer.toString(Calendar.getInstance().get(Calendar.MINUTE));
+
+    }
 }
